ae5bace87545c002adbd5ae7da998cae005cb4ef
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2022-10-13 12:50:55 -0600

    Added a basic use case test

--- Assets/ARSIS/Core/EventManager/Tester.cs
+++ Assets/ARSIS/Core/EventManager/Tester.cs
@@ -7,11 +7,25 @@ public class Tester : MonoBehaviour {
         EventManager.AddListener((TestArsisIntEvent e) => {
             Debug.Log("The value is: " + (int)e);
         });
+        EventManager.AddListener(TestOxygenUpdateHandler);
     }
 
     void Update() {
         if (Keyboard.current.spaceKey.wasPressedThisFrame) {
             EventManager.Trigger(new TestArsisIntEvent(2));
         }
+
+        //every secondish randomly post a new oxygen level between 0 and 100
+        if (Time.time % 1 < Time.deltaTime) {
+            EventManager.Trigger(new OxygenLevel(Random.Range(0, 100)));
+        }
+    }
+
+    private void thisIsAnotherTest(TestArsisIntEvent e) {
+        Debug.Log("twice the value is: " + e * 2);
+    }
+
+    private void TestOxygenUpdateHandler(OxygenLevel e) {
+        Debug.Log("Oxygen level is: " + e);
     }
 }
