b3b3256084c2dc4299c7f399ea7f01f8137327c5
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2022-10-13 11:12:52 -0600

    Updated for new input system

--- Assets/ARSIS/Core/EventManager/Tester.cs
+++ Assets/ARSIS/Core/EventManager/Tester.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 using EventManager;
+using UnityEngine.InputSystem;
+
 public class Tester : MonoBehaviour {
     void Start() {
         EventManager.EventManager.AddListener((TestArsisIntEvent e) => {
             Debug.Log("Hello");
         });
+    }
+
+    void Update() {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+            EventManager.EventManager.Trigger(new TestArsisIntEvent(2));
+        }
     }
 }
