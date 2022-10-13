f23a2a32b77f03d806b9b3b23fbc2b92586a480b
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2022-10-13 14:35:32 -0600

    Added docstrings to EventManager for testing

--- Assets/ARSIS/Core/EventManager/EventManager.cs
+++ Assets/ARSIS/Core/EventManager/EventManager.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 
 namespace EventManagerSystem
 {
+    /// <summary>
+    /// This class contains the methods that are used to 
+    /// add, remove, and trigger events. It is a static class
+    /// so that it can be accessed from anywhere. All of the
+    /// events should be of type (subclass of
+    /// IArsisEvent.)
+    /// </summary>
     public static class EventManager
     {
         private static Dictionary<Delegate, Action<dynamic>> wrappers = new Dictionary<Delegate, Action<dynamic>>();
@@ -20,6 +27,12 @@ namespace EventManagerSystem
             }
         }
 
+        /// <summary>
+        /// This adds a listener to the provided event type.
+        /// </summary>
+        /// <typeparam name="EventType">This is the type of the Event to add a listener to</typeparam>
+        /// <param name="eventFunction">The Listener that will be called when the event is called.</param>
+        /// <exception cref="Exception">That Listener is already listeneing to this event</exception>
         public static void AddListener<EventType>(Action<EventType> eventFunction) where EventType : IArsisEvent {
             if (wrappers.ContainsKey(eventFunction)) {
                 //This is just for demonstaration, we should change the error type
@@ -29,6 +42,11 @@ namespace EventManagerSystem
             AddListener(wrappers[eventFunction], typeof(EventType));
         }
 
+        /// <summary>
+        /// This removes an event listener from the provided event type.
+        /// </summary>
+        /// <typeparam name="EventType">The event type to remove the listener from.</typeparam>
+        /// <param name="eventFunction">The lister to remove.</param>
         public static void RemoveListener<EventType>(Action<EventType> eventFunction) where EventType : IArsisEvent
         {
             if (eventDictionary.ContainsKey(typeof(EventType)))
@@ -38,7 +56,11 @@ namespace EventManagerSystem
             }
             //Might want to add an error if it does not exist or was already removed
         }
-
+        
+        /// <summary>
+        /// This triggers events that have the same datatype as the provided data using the provided data.
+        /// </summary>
+        /// <param name="data">The data to pass to events</param>
         public static void Trigger(dynamic data)
         {
             if (eventDictionary.ContainsKey(data.GetType()))
