cba102da4caee581baef3a145c05b5fd1ebb1781
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2022-10-20 10:45:13 -0600

    Added empty tests for the Event System

---
+++ Assets/Tests/EventSystemTests.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using EventSystem;
+
+public class EventSystemTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void EventSystemTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void SecondTest()
+    {
+        // Use the Assert class to test conditions
+    }
+}
