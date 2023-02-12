7ba06124002d5d4b29d985fb680fef8ef320cc2e
chuck <c.burnell@gmail.com>
2023-02-12 12:42:45 -0700

    wip

--- ARSIS-Telemetry-GroundControl/telemetry-api/app/main.py
+++ ARSIS-Telemetry-GroundControl/telemetry-api/app/main.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# TODO: Move this into a locations module
+@app.get("/location/")
+async def location():
+    return {"lat": 10, "lon": 10}
+
+
+# TODO: Move this into a biometrics module
+@app.get("/biometrics/")
+async def biometrics():
+    return {"hr": 75, "o2": 0.9}
