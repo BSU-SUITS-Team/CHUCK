1fed40ee2823df72fa40988bfe93bf992457bc82
chuck <c.burnell@gmail.com>
2023-02-12 12:19:53 -0700

    added git ignore for postgres data folder

---
+++ ARSIS-Telemetry-GroundControl/groundcontrol-fe/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import TaskRow from './components/TaskRow';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>landing page</h1>
+      <TaskRow></TaskRow>
+    </div>
+  );
+}
+
+export default App;
