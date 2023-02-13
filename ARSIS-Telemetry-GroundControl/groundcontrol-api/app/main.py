a15eb97e0a3fdef2a17cc4e145d797fce10acefb
chuck <c.burnell@gmail.com>
2023-02-12 22:55:53 -0700

    cruft removal

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
 from pydantic import BaseModel
+import random
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Ground Control API"}
+
+
+@app.get("/fake-procedure")
+def fake_procedure():
+    return {"name": "procedure 1", "number_of_tasks": 5}
+
+
+@app.get("/fake-task")
+def fake_task():
+    to_return = []
+    for i in range(random.randint(1, 7)):
+        to_return.append(["text", "space " * random.randint(6, 15)])
+    return {"task_items": to_return}
 
 
 class LoggingRequest(BaseModel):
