708176a0f8f58b2cdc33098c9e9ba427fdf9dd9b
chuck <c.burnell@gmail.com>
2023-02-14 18:31:02 -0700

    removing unneeded endpoint for this issue

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
@@ -4,22 +4,10 @@ import random
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Ground Control API"}
-
-
-@app.get("/fake-procedure")
-def fake_procedure():
-    return {"name": "procedure 1", "number_of_tasks": 5}
-
-
-@app.get("/fake-task")
-def fake_task():
-    to_return = []
-    for i in range(random.randint(1, 7)):
-        to_return.append(["text", "space " * random.randint(6, 15)])
-    return {"task_items": to_return}
 
 
 class LoggingRequest(BaseModel):
