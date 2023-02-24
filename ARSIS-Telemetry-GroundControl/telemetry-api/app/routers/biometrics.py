d9ac69050c9f60aa5c897a3b6dbf88c7eb0a79a6
Isaac Denny <isaacquinndenny@gmail.com>
2023-02-24 13:40:31 -0700

    finished/tested biometrics endpoints added specific bio requests to TelemetryClient.cs

--- ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/biometrics.py
+++ ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/biometrics.py
@@ -3,21 +3,46 @@ import random
 
 router = APIRouter()
 
-bpm = random.random(95, 162)
+bpm = 120
 o2 = 100
 battery = 100
 
-o2_drop_chance = 50
-battery_drop_chance = 50
+o2_drop_chance = 25
+battery_drop_chance = 25
 
-@router.get("/biometrics/", tag=["biometrics"])
+def evaluate_biometrics():
+    global bpm
+    global battery
+    global o2
+
+    bpm = random.randrange(95, 162)
+    if random.randint(0, 100) < battery_drop_chance and battery > 0:
+        battery -= random.randrange(0, 2)
+    elif battery <= 0:
+        battery = 0
+    if random.randint(0, 100) < o2_drop_chance and o2 > 0:
+        o2 -= random.randrange(0, 2)
+    elif o2 <= 0:
+        o2 = 0
+
+@router.get("/biometrics/", tags=["biometrics"])
 async def get_biometrics():
-    if random.randint(0, 100) < battery_drop_chance:
-        battery -= random.random(0, 2)
-    if random.randint(0, 100) < o2_drop_chance:
-        o2 -= random.random(0, 2)
+    evaluate_biometrics()
     return {"bpm": bpm, "o2": o2, "battery": battery}
 
-@router.get("/biometrics/{data_type}", tag=["biometrics"])
+@router.get("/biometrics/{data_type}", tags=["biometrics"])
 async def get_biometrics(data_type: str):
-    return { "{data_type}": data_type }
+    evaluate_biometrics()
+    if not data_type:
+        return {"message": "No data type specified"}
+    match data_type:
+        case "bpm":
+            return { "bpm": bpm }
+        case "o2":
+            return { "o2": o2 }
+        case "battery":
+            return { "battery": battery }
+        case _:
+            return {"error": "Invalid data type"}
+        
+        
