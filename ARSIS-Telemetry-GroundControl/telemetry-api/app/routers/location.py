41bef039ef62decc1e52271950b978e4ee3540c0
chuck <c.burnell@gmail.com>
2023-03-05 17:29:30 -0700

    wip

--- ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/location.py
+++ ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/location.py
@@ -1,19 +1,30 @@
-from fastapi import APIRouter
-import string
 import random
+import string
 
-router = APIRouter(
-    prefix="/location",
-    tags=["location"]
-)
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/location", tags=["location"])
 
 random.seed(20031101)
 
+
+class LocationLLAH(BaseModel):
+    latitude: float
+    longitude: float
+    altitude: float
+    heading: float
+
+
 def create_random_location(
-    latitude_min = -90, latitude_max = 90, 
-    longitude_min = -180, longitude_max = 180, 
-    altitude_min = 420, altitude_max = 8848, 
-    heading_min = 0, heading_max = 360
+    latitude_min=-90,
+    latitude_max=90,
+    longitude_min=-180,
+    longitude_max=180,
+    altitude_min=420,
+    altitude_max=8848,
+    heading_min=0,
+    heading_max=360,
 ):
     latitude = round(random.uniform(latitude_min, latitude_max), 6)
     longitude = round(random.uniform(longitude_min, longitude_max), 6)
@@ -21,35 +32,43 @@ def create_random_location(
     heading = random.randint(heading_min, heading_max)
     return (latitude, longitude, altitude, heading)
 
-def create_random_string(characters = string.ascii_lowercase):
-    return ''.join(random.choice(characters) for _ in range(6))
+
+def create_random_string(characters=string.ascii_lowercase):
+    return "".join(random.choice(characters) for _ in range(6))
+
 
 @router.get("/")
 async def location():
 
-    data = {
-        "users": []
-    }
+    data = {"users": []}
 
     for _ in range(random.randint(1, 20)):
         (latitude, longitude, altitude, heading) = create_random_location()
-        data["users"].append({
-            "user": create_random_string(),
-            "latitude": latitude,
-            "longitude": longitude,
-            "altitude": altitude,
-            "heading": heading
-        })
+        data["users"].append(
+            {
+                "user": create_random_string(),
+                "latitude": latitude,
+                "longitude": longitude,
+                "altitude": altitude,
+                "heading": heading,
+            }
+        )
 
     return data
 
+
 @router.get("/{user}")
-async def location(user: str):
+async def user_location(user: str):
     (latitude, longitude, altitude, heading) = create_random_location()
 
     return {
         "latitude": latitude,
         "longitude": longitude,
         "altitude": altitude,
-        "heading": heading
+        "heading": heading,
     }
+
+
+@router.post("/{user}/location")
+async def update_user_location(request: Request, user: str, new_location: LocationLLAH):
+    request.app.user_cache.update_location(user, new_location)
