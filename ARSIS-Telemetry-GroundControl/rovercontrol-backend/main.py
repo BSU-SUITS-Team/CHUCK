1638df8667b7799ed2b67a19894973d60c790ef8
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-03-09 13:05:34 -0700

    Comments

--- ARSIS-Telemetry-GroundControl/rovercontrol-backend/main.py
+++ ARSIS-Telemetry-GroundControl/rovercontrol-backend/main.py
@@ -13,39 +13,65 @@ recieved_data_queue = multiprocessing.Queue()
 unity_information_queue = multiprocessing.Queue()
 rover_information_queue = multiprocessing.Queue()
 
-def execute_key_listener(data):
+
+def execute_key_listener(data: dict) -> None:
+    """
+    This function is used to execute the key listeners that are registered.
+
+    Key listeners are functions that are registered to be executed when a certain key is recieved from the rover or unity.
+    Their argument is the json object that is recieved from the rover or unity.
+
+    Args:
+        data: The json object that is recieved from the rover or unity.
+    """
     for key in key_listeners:
         if key in data.keys():
             key_listeners[key](data[key])
 
-def parse_unity_instructions(data) -> dict:
+
+def parse_unity_instructions(data: str) -> dict:
+    """
+    This function is used to parse the data that is recieved from the unity and rover.
+
+    Args:
+        data: The data that is recieved from the unity and rover.
+    """
     return json.loads(data)
 
-def EvaluateData():
+
+def EvaluateData() -> None:
+    """
+    This function is used to evaluate the data that is recieved from the rover and unity.
+    """
     while True:
         if not recieved_data_queue.empty():
             data = recieved_data_queue.get()
             execute_key_listener(data)
 
-def UnityServer():
+
+def UnityServer() -> None:
     print('Unity server started')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, UNITY_PORT))
         s.listen()
         while True:
             conn, addr = s.accept()
-            multiprocessing.Process(target=Connection, args=(conn, addr)).start()
+            multiprocessing.Process(
+                target=Connection, args=(conn, addr)).start()
 
-def RoverServer():
+
+def RoverServer() -> None:
     print('Rover server started')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, ROVER_PORT))
         s.listen()
         while True:
             conn, addr = s.accept()
-            multiprocessing.Process(target=Connection, args=(conn, addr)).start()
+            multiprocessing.Process(
+                target=Connection, args=(conn, addr)).start()
 
-def Connection(conn, addr):
+
+def Connection(conn, addr) -> None:
     with conn:
         print('New connection from ', addr)
         data = b''
@@ -61,15 +87,15 @@ def Connection(conn, addr):
         conn.sendall(json.dumps(to_send).encode('utf-8'))
 
 
-
-
-
 if __name__ == '__main__':
+    parser_process = multiprocessing.Process(target=EvaluateData)
     unity_process = multiprocessing.Process(target=UnityServer)
     rover_process = multiprocessing.Process(target=RoverServer)
+
+    # Start the processes and wait for them to finish. This should take forever as the processes should never end.
+    parser_process.start()
     unity_process.start()
     rover_process.start()
     unity_process.join()
     rover_process.join()
-
-            
+    parser_process.join()
