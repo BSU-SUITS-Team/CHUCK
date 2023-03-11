b411164d394eb79c1474ee2500413152a407181d
Aetheris743 <you@example.com>
2023-03-11 13:33:04 -0700

    Added setup for raspberry pi

--- ARSIS-Devices/VisionKit/control_endpoint/run.sh
+++ ARSIS-Devices/VisionKit/control_endpoint/run.sh
@@ -1,1 +1,2 @@
-poetry run uvicorn app.main:app --host 0.0.0.0 --port 8282
+cd /home/arsis/ARSIS-6/ARSIS-Devices/VisionKit/control_endpoint
+uvicorn app.main:app --host 0.0.0.0 --port 8282
--- ARSIS-Devices/VisionKit/control_endpoint/setup.sh
+++ ARSIS-Devices/VisionKit/control_endpoint/setup.sh
@@ -1,2 +1,16 @@
-curl -sSL https://install.python-poetry.org | python3 -
-poetry install  --no-interaction --no-ansi --no-root
+#setup python 3.10
+wget https://www.python.org/ftp/python/3.10.7/Python-3.10.7.tgz
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev
+apt-get install libffi-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+tar -xzvf Python-3.10.7.tgz 
+cd Python-3.10.7/
+./configure --enable-optimizations
+make install
+ln -sf $(which python3) /usr/bin/python
+
+#setup project
+# sudo -u arsis curl -sSL https://install.python-poetry.org | python3 -
+# export PATH="/home/arsis/.local/bin:$PATH"
+# poetry install  --no-interaction --no-ansi --no-root
+python -m pip install pyserial fastapi uvicorn
+echo @reboot sh /home/arsis/ARSIS-6/ARSIS-Devices/VisionKit/control_endpoint/run.sh > /etc/cron.d/serverstart
