b9947cf351086a62a4b5cdca1d314feaf8ada2ca
Isaac Denny <isaacquinndenny@gmail.com>
2023-03-16 15:14:16 -0600

    env and sql fixes

--- ARSIS-Telemetry-GroundControl/groundcontrol-db/database.sql
+++ ARSIS-Telemetry-GroundControl/groundcontrol-db/database.sql
@@ -4,4 +4,30 @@ CREATE TABLE logs (
   data JSON NOT NULL
 );
 
-INSERT INTO logs (timeIn, data) VALUES (now(), '{"test log": "This is a test log"}'), (now(), '{"test log": "This is another test log"}');
+CREATE TABLE biometrics (
+  uuid SERIAL PRIMARY KEY NOT NULL,
+  timein TIMESTAMP DEFAULT now(),
+  user_id VARCHAR(20) NOT NULL,
+  o2 INTEGER NOT NULL,
+  battery INTEGER NOT NULL,
+  bpm INTEGER NOT NULL
+);
+
+CREATE TABLE location (
+  uuid SERIAL PRIMARY KEY NOT NULL,
+  timein TIMESTAMP DEFAULT now(),
+  user_id VARCHAR(20) NOT NULL,
+  latitude INTEGER NOT NULL,
+  longitude INTEGER NOT NULL,
+  altitude INTEGER NOT NULL,
+  heading INTEGER NOT NULL
+);
+
+CREATE TABLE users (
+  uuid SERIAL PRIMARY KEY NOT NULL,
+  user_id VARCHAR(20) UNIQUE NOT NULL
+);
+
+-- INSERT INTO users (userid, callsign, firstname, lastname) VALUES (101, "Maverick", "Pete", "Mitchel"), (102, "Boxman", "Jim", "Boxman"), (103, "Goose", "Nick", "Bradshaw");
+-- INSERT INTO biometrics (timein, userid, o2, battery, bpm) VALUES (now(), 101, 140, 100, 100), (now(), 102, 155, 100, 100), (now(), 103, 167, 100, 100);
+-- INSERT INTO location (timein, userid, latitude, longitude, altitude, heading) VALUES (now(), 101, 130, 100, 100, 100), (now(), 102, 120, 100, 100, 100), (now(), 103, 140, 100, 100, 100);
