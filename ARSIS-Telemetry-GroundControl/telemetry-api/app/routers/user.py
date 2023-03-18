a95c2ac384be8e605ecc3868ee6b5c90fb0d8105
dignojrteogalbo <dignojrteogalbo@gmail.com>
2023-03-17 22:48:09 -0600

    users endpoint now uses postgresql database
    will need to update other endpoints to use postgresql database

--- ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/user.py
+++ ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/user.py
@@ -1,25 +1,43 @@
-from fastapi import APIRouter, Request, status
+import datetime
+from fastapi import APIRouter, Request, Response, status
+from pydantic import BaseModel
+from app.database import connection
 
 router = APIRouter(prefix="/user", tags=["user"])
 
+class User(BaseModel):
+    name: str
 
 @router.get("/")
-def get_all_users(request: Request):
-    return request.app.user_cache.get_all()
-
+def get_all_users():
+    with connection.cursor() as db:
+        db.execute("SELECT * FROM users;")
+        result = db.fetchall()
+        response = []
+        for row in result:
+            (id, name, createdAt) = row
+            response.append({ "id": id, "name": name, "createdAt": createdAt })
+        return { "users": response }
 
 @router.get("/{user}")
-async def get_user(request: Request, user: str):
+async def get_user(user: str):
+    try:
+        user = int(user)
+    except Exception:
+        return status.HTTP_400_BAD_REQUEST
 
-    to_return = request.app.user_cache.get(user)
-    to_return = to_return if to_return is not None else status.HTTP_404_NOT_FOUND
-    return to_return
+    with connection.cursor() as db:
+        db.execute("SELECT * FROM users WHERE id = %s;", (user,))
+        (id, name, createdAt) = db.fetchone()
+        if (id and name and createdAt):
+            return { "id": id, "name": name, "createdAt": createdAt }
+        else:
+            return status.HTTP_404_NOT_FOUND
 
+@router.put("/")
+async def put_user(user: User):
+    with connection.cursor() as db:
+        db.execute("INSERT INTO users (name) VALUES (%s) RETURNING *;", (user.name,))
+        (id, name, createdAt) = db.fetchone()
+        return { "id": id, "name": name, "createdAt": createdAt}
 
-@router.put("/{user}")
-async def put_user(request: Request, user: str):
-    user_id = request.app.user_cache.register(user)
-    to_return = status.HTTP_201_CREATED
-    if user_id is None:
-        to_return = status.HTTP_409_CONFLICT
-    return to_return
