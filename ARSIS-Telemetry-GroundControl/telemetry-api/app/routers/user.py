7044929740eadee2077994c8f8e938b32ec4ddaf
chuck <c.burnell@gmail.com>
2023-03-19 20:13:26 -0600

    fixed location to use db and added some uuid primary keys for locations and biometrics

--- ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/user.py
+++ ARSIS-Telemetry-GroundControl/telemetry-api/app/routers/user.py
@@ -1,12 +1,13 @@
-import datetime
-from fastapi import APIRouter, Request, Response, status
+from fastapi import APIRouter, status
 from pydantic import BaseModel
 from app.database import connection
 
 router = APIRouter(prefix="/user", tags=["user"])
 
+
 class User(BaseModel):
     name: str
+
 
 @router.get("/")
 def get_all_users():
@@ -16,11 +17,12 @@ def get_all_users():
         response = []
         for row in result:
             (id, name, createdAt) = row
-            response.append({ "id": id, "name": name, "createdAt": createdAt })
-        return { "users": response }
+            response.append({"id": id, "name": name, "createdAt": createdAt})
+        return {"users": response}
+
 
 @router.get("/{user}")
-async def get_user(user: str):
+async def get_user(user: int):
     try:
         user = int(user)
     except Exception:
@@ -29,14 +31,16 @@ async def get_user(user: str):
     with connection.cursor() as db:
         db.execute("SELECT * FROM users WHERE id = %s;", (user,))
         (id, name, createdAt) = db.fetchone()
-        if (id and name and createdAt):
-            return { "id": id, "name": name, "createdAt": createdAt }
+        if id and name and createdAt:
+            return {"id": id, "name": name, "createdAt": createdAt}
         else:
             return status.HTTP_404_NOT_FOUND
+
 
 @router.put("/")
 async def put_user(user: User):
     with connection.cursor() as db:
         db.execute("INSERT INTO users (name) VALUES (%s) RETURNING *;", (user.name,))
         (id, name, createdAt) = db.fetchone()
-        return { "id": id, "name": name, "createdAt": createdAt}
+        connection.commit()
+        return {"id": id, "name": name, "createdAt": createdAt}
