340a65b758ed51a5e6649f220986d2cdd6fb3fc3
chuck <c.burnell@gmail.com>
2023-03-28 13:26:58 -0600

    forgot an ignore file :(

--- ARSIS-Rover/ros/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src
-# Build directory: /home/ubuntu/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("vision_kit")
