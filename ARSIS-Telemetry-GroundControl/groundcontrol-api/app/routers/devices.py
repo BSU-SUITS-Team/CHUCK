e33b5a7f2cfc6ae27cf8cd6e5e19943b32205ae0
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-03-30 10:55:25 -0600

    Update devices.py
    Added docstrings

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/routers/devices.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/routers/devices.py
@@ -9,19 +9,42 @@ router = APIRouter(prefix="/devices", tags=["devices"])
 
 devices = {}
 
+
 @router.get("/get_configured")
 async def return_all_configured_devices():
+    """This function returns a list of all configured devices.
+
+    Returns:
+        (dict): A dict of all configured devices.
+    """
     return devices
 
-@router.get("/get_configured/{name}")
+
+@router.get("/is_configured/{name}")
 async def return_configured_device(name: str):
+    """This function returns whether or not a device is configured.
+
+    Args:
+        name (str): The name of the device to be checked.
+
+    Returns:
+        (bool): True if the device is configured, False if it is not."""
     return name in devices.keys()
+
 
 @router.post("/configure/{name}")
 async def configure_device(name: str, ip_address: str):
+    """This function configures a device to be controlled by ground control.
+
+    Args:
+        name (str): The name of the device to be configured. This can be arbitrary.
+        ip_address (str): The ip address of the device to be configured. This is used to send commands to the device.
+
+    Returns:
+        (int): The status code of the request. 200 if the device was configured successfully, 400 if the ip address was invalid or the device was already configured.
+    """
     if ip_address is None:
         return 400
-    
     try:
         ip_address = ipaddress.ip_address(ip_address)
         devices[name] = ip_address
@@ -29,28 +52,66 @@ async def configure_device(name: str, ip_address: str):
         return 400
     return 200
 
+
 @router.delete("/remove/{name}")
 async def remove_device(name: str):
+    """This function removes a device from the list of configured devices.
+
+    Args:
+        name (str): The name of the device to be removed.
+
+    Returns:
+        (int): The status code of the request. 200 if the device was removed successfully, 400 if the device was not configured.
+    """
     if name in devices.keys():
         del devices[name]
         return 200
     return 400
 
+
 @router.post("/broadcast")
 async def broadcast_command(command: str):
+    """This function sends a command to all configured devices.
+
+    Args:
+        command (str): The command to be sent to the devices. This is transmitted as a byte string.
+
+    Returns:
+        (int): The status code of the request. 200 if the command was sent successfully.
+    """
     for device in devices:
         requests.put("http://" + str(devices[device]) + ":8282/" + command)
     return 200
 
+
 @router.post("/send_command")
 async def send_command(names: list[str], command: str):
+    """This function sends a command to a list of configured devices.
+
+    Args:
+        names (list[str]): A list of the names of the devices to send the command to.
+        command (str): The command to be sent to the devices. This is transmitted as a byte string.
+
+    Returns:
+        (int): The status code of the request. 200 if the command was sent successfully, 404 if one or more of the devices were not configured.
+    """
     for name in names:
         if name in devices.keys():
             requests.put("http://" + str(devices[name]) + ":8282/" + command)
     return 200
 
+
 @router.post("/send_command/{name}")
 async def send_command(name: str, command: str):
+    """This function sends a command to a configured device.
+
+    Args:
+        name (str): The name of the device to send the command to.
+        command (str): The command to be sent to the device. This is transmitted as a byte string.
+
+    Returns:
+        (int): The status code of the request. 200 if the command was sent successfully, 404 if the device was not configured.
+    """
     if name in devices.keys():
         requests.put(devices[name] + "/" + command)
         return 200
