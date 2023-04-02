132c418153ca5fe32f5aa7f366d7b346917a16d7
chuck <c.burnell@gmail.com>
2023-04-02 12:50:50 -0600

    wip

--- ARSIS-Rover/ros/ros_entrypoint.sh
+++ ARSIS-Rover/ros/ros_entrypoint.sh
@@ -4,4 +4,11 @@ source /opt/ros/noetic/setup.bash
 cd /catkin_ws/
 catkin_make
 source ./devel/setup.bash
-roslaunch vision_kit vision_kit.launch
+echo $VISION_KIT_MODE
+if [[ "${VISION_KIT_MODE}" == "PROTOTYPE" ]]; then
+    roslaunch vision_kit vision_kit_prototype.launch
+elif [[ "${VISION_KIT_MODE}" == "VISION" ]]; then
+    roslaunch vision_kit vision_kit_vision.launch
+elif [[ "${VISION_KIT_MODE}" == "ROVER" ]]; then
+    roslaunch vision_kit vision_kit_rover.launch
+fi
