3591fad769f497d7fbb48a095dec552ce473b7f8
Isaac Denny <isaacquinndenny@gmail.com>
2023-04-05 12:29:59 -0600

    working procedure menu

--- ARSIS-Telemetry-GroundControl/groundcontrol-fe/src/pages/Procedures.js
+++ ARSIS-Telemetry-GroundControl/groundcontrol-fe/src/pages/Procedures.js
@@ -1,22 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SideNav from "../components/SideNav";
 
 const Procedures = () => {
-  const [newProcedure, setNewProcedure] = useState({});
-  const [tasks, setTasks] = useState([{ name: "", summary: "", stepList: "" }]);
-  const [steps, setSteps] = useState([{ type: "", body: "", nextTask: "" }]);
+  const defaultProc = {
+    name: "",
+    summary: "",
+    taskList: [
+      {
+        name: "",
+        summary: "",
+        stepList: [{ type: "", body: "", nextTask: "" }],
+      },
+    ],
+  };
+  const [newProcedure, setNewProcedure] = useState(defaultProc);
 
-  const handleCreate = () => {
-    console.log("created procedure");
+  const handleCreate = (e) => {
+    // e.preventDefault();
+    // procedureData.name = e.target["proc-name"].value;
+    // procedureData.summary = e.target["proc-summary"].value;
   };
 
   const handleAddTask = () => {
-    setTasks([...tasks, { name: "", summary: "", stepList: "" }]);
+    let procedureData = { ...newProcedure };
+    procedureData.taskList.push({
+      name: "",
+      summary: "",
+      stepList: [{ type: "", body: "", nextTask: "" }],
+    });
+    setNewProcedure(procedureData);
   };
 
-  const handleAddStep = () => { 
-    console.log("added step");
-  }
+  const handleRemoveTask = () => {
+    let procedureData = { ...newProcedure };
+    procedureData.taskList.pop();
+    setNewProcedure(procedureData);
+  };
+
+  const handleAddStep = (index) => {
+    let procedureData = { ...newProcedure };
+    procedureData.taskList[index].stepList.push({
+      type: "",
+      body: "",
+      nextTask: "",
+    });
+    setNewProcedure(procedureData);
+  };
+  const handleRemoveStep = (index) => {
+    let procedureData = { ...newProcedure };
+    procedureData.taskList[index].stepList.pop();
+    setNewProcedure(procedureData);
+  };
+
+  useEffect(() => {
+    console.log(newProcedure);
+  }, [setNewProcedure])
 
   return (
     <>
@@ -25,30 +63,70 @@ const Procedures = () => {
         <h1>Procedures</h1>
         <p>Ground Control Panel</p>
         <div className="Container-primary">
-          <form onSubmit={handleCreate}>
+          <form onSubmit={() => handleCreate}>
             <h3>Create Procedure</h3>
             <label>Procedure Details</label>
             <input type="text" name="proc-name" placeholder="Name" />
             <input type="text" name="proc-summary" placeholder="Summary" />
             <label>Tasks</label>
-            <div>
-              {tasks.length > 0 ? (
-                tasks.map((task, i) => {
+            <div className="Task-list" style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
+              {newProcedure.taskList.length > 0 ? (
+                newProcedure.taskList.map((task, i) => {
                   return (
-                    <div key={i} className='Pretty-form'>
+                    <div key={i} className="Pretty-form">
                       <label>Task {i + 1}</label>
                       <input
                         type="text"
-                        name="task-name"
+                        name={"task-name" + i}
+                        defaultValue={task.name}
                         placeholder="Name"
-                        value={task.name}
                       />
                       <input
                         type="text"
-                        name="task-summary"
+                        name={"task-summary" + i}
+                        defaultValue={task.summary}
                         placeholder="Summary"
-                        value={task.summary}
                       />
+                      <label>Steps</label>
+                      <div className="Step-list">
+                        {newProcedure.taskList[i].stepList.length > 0 ? (
+                          newProcedure.taskList[i].stepList.map((step, j) => {
+                            return (
+                              <div key={j} className="Pretty-form">
+                                <label>Step {j + 1}</label>
+                                <input
+                                  type="text"
+                                  name={"step-type" + j}
+                                  defaultValue={step.type}
+                                  placeholder="Type"
+                                />
+                                <input
+                                  type="text"
+                                  name={"step-body" + j}
+                                  defaultValue={step.body}
+                                  placeholder="Body"
+                                />
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <p>No Steps</p>
+                        )}
+                        <div className="Button-group">
+                          <button
+                            type="button"
+                            onClick={() => handleAddStep(i)}
+                          >
+                            Add Step
+                          </button>
+                          <button
+                            type="button"
+                            onClick={() => handleRemoveStep(i)}
+                          >
+                            Remove Step
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   );
                 })
@@ -59,6 +137,9 @@ const Procedures = () => {
             <div className="Button-group">
               <button type="button" onClick={handleAddTask}>
                 Add Task
+              </button>
+              <button type="button" onClick={handleRemoveTask}>
+                Remove Task
               </button>
               <button type="submit">Create</button>
             </div>
