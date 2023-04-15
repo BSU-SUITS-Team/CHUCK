bd2e5a54f52be416de3db419aa5b851a86d2ddc3
Isaac Denny <isaacquinndenny@gmail.com>
2023-04-15 15:51:29 -0600

    fixed cors issue and upload image bug

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import procedures
 from app.routers import logs
 
 app = FastAPI()
 app.include_router(procedures.router)
 app.include_router(logs.router)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
--- ARSIS-Telemetry-GroundControl/telemetry-api/app/main.py
+++ ARSIS-Telemetry-GroundControl/telemetry-api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import biometrics, location, user, uia
 from app.caches.user_cache import UserCache
 
@@ -7,6 +8,18 @@ app.include_router(location.router)
 app.include_router(biometrics.router)
 app.include_router(user.router)
 app.include_router(uia.router)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
