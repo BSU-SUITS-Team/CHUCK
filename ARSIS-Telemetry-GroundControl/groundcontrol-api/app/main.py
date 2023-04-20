d426eb8ce75ec10da8842deef6278fb8e0bf91f2
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-04-20 13:03:06 -0600

    Sending Messages and getting them works

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.routers import procedures
-from app.routers import logs
+from app.routers import procedures, logs, chat
 
 app = FastAPI()
 app.include_router(procedures.router)
 app.include_router(logs.router)
+app.include_router(chat.router)
 
 @app.get("/")
 async def root():
