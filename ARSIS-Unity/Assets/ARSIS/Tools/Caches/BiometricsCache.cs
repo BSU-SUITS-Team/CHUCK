4045d9921166d5e98e11e6c844177c9f80824b17
chuck <c.burnell@gmail.com>
2023-05-08 13:41:49 -0600

    checks biometrics event for out of bound ranges

--- ARSIS-Unity/Assets/ARSIS/Tools/Caches/BiometricsCache.cs
+++ ARSIS-Unity/Assets/ARSIS/Tools/Caches/BiometricsCache.cs
@@ -6,12 +6,14 @@ using ARSISEventSystem;
 public class BiometricsCache : MonoBehaviour
 {
 
-    public BiometricsEvent BiometricsEvent;
+    public BiometricsEvent biometricsEvent;
     private static int MINUTES = 5;
     private static int MAX_ENTRYS = 60*MINUTES;
     private List<BiometricsEvent> BiometricsList;
     public static BiometricsCache BiometricsCacheSingleton { get; private set; }
     public int Count;
+    public int OutOfRangeCount;
+    public HashSet<string> outOfRangeBiometrics;
     private void Awake()
     {
         BiometricsList = new List<BiometricsEvent>();
@@ -24,21 +26,38 @@ public class BiometricsCache : MonoBehaviour
         {
             BiometricsCacheSingleton = this;
             EventManager.AddListener<BiometricsEvent>(UpdateBiometrics);
+            outOfRangeBiometrics = new HashSet<string>();
         }
     }
     void UpdateBiometrics(BiometricsEvent be){
-        BiometricsEvent = be;
+        biometricsEvent = be;
+        CheckBiometrics(biometricsEvent);
         if (BiometricsList.Count > MAX_ENTRYS){
             BiometricsList.RemoveAt(0);
         }
         BiometricsList.Add(be);
         Count = BiometricsList.Count;
+        OutOfRangeCount = outOfRangeBiometrics.Count;
+    }
+
+    void CheckBiometrics(BiometricsEvent be){
+
+        foreach((int rate, int[] limits, string name) in be.intCheckList){
+            if(rate < limits[0] || rate > limits[1]){
+                outOfRangeBiometrics.Add(name);
+            }
+        }
+        foreach((bool status, bool nominal, string name) in be.boolCheckList){
+            if(status != nominal){
+                outOfRangeBiometrics.Add(name);
+            }
+        }
     }
 
     public float getHeartrate(){
         float heartrate = 0;
-        if(BiometricsEvent != null){
-            heartrate = BiometricsEvent.heartrate;
+        if(biometricsEvent != null){
+            heartrate = biometricsEvent.heartrate;
         }
         return heartrate;
     }
@@ -65,5 +84,8 @@ public class BiometricsCache : MonoBehaviour
             batteryList.Add(biometricsEvent.battery);
         }
         return batteryList;
+    }
+    public void acknowledgeOutOfRange(string name){
+        outOfRangeBiometrics.Remove(name);
     }
 }
