4a7e7be023752eacb5a0adee939ff80a49f29f1d
dignojr <dignojrteogalbo@gmail.com>
2024-01-14 13:36:02 -0700

    create tss module

---
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/tss.py
@@ -0,0 +1,12 @@
+endpoint = "localhost:14141"
+get = {
+    "telemetry" : "/json_data/teams/0/TELEMETRY.json",
+    "rover"     : "/json_data/ROVER.json",
+    "imu"       : "/json_data/IMU.json",
+    "uia"       : "/json_data/UIA.json",
+    "dcu"       : "/json_data/DCU.json",
+    "eva"       : "/json_data/teams/0/EVA.json",
+}
+
+def get_telemetry():
+    pass
