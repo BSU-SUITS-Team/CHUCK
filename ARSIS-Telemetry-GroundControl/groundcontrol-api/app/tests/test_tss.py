e2cd705feafebfcd4aa83a441fcc9594de7d967d
dignojr <dignojrteogalbo@gmail.com>
2024-01-14 13:40:06 -0700

    get imu test

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/tests/test_tss.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/tests/test_tss.py
@@ -1,13 +1,13 @@
 import requests
 import responses
 import json
-from app.tss import endpoint, get, get_telemetry
+from app.tss import endpoint, get, get_telemetry, get_imu
 
 class TestTSSRequests:
 
     @responses.activate
     def test_get_telemetry(self):
-        with open("app/tests/sample_telemetry.json") as file:
+        with open("app/tests/json/sample_telemetry.json") as file:
             body = json.load(file)
             responses.add(**{
                 "method": responses.GET,
@@ -17,4 +17,18 @@ class TestTSSRequests:
                 "content_type": "application/json",
             })
             response = get_telemetry()
+            assert response.status_code == 200
+    
+    @responses.activate
+    def test_get_imu(self):
+        with open("app/tests/json/sample_imu.json") as file:
+            body = json.load(file)
+            responses.add(**{
+                "method": responses.GET,
+                "url": endpoint + get["imu"],
+                "body": str(body),
+                "status": 200,
+                "content_type": "application/json",
+            })
+            response = get_imu()
             assert response.status_code == 200
