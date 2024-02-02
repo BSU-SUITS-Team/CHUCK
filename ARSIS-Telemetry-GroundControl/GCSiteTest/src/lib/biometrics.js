e362ec2194351c2badd6638d9a86ae32cc1b7853
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2024-02-02 11:19:20 -0700

    Update biometrics.js

--- ARSIS-Telemetry-GroundControl/GCSiteTest/src/lib/biometrics.js
+++ ARSIS-Telemetry-GroundControl/GCSiteTest/src/lib/biometrics.js
@@ -1,29 +1,27 @@
-import { readable, writable } from 'svelte/store';
+import { readable } from 'svelte/store';
 
-export const numberofastronauts = writable(1);
-export const biometics = readable(
-    {
-        name: 'EV-1',
-        oxygen: 99,
-        battery: 14,
-        co2: 178,
-        heartRate: 90,
-        temperature: 98,
-        location: 'Mars',
-        fanSpeed: 68
-    },
+export const numberofastronauts = 3
+function getastros() {
+    let astronauts = []
+    for (let i = 0; i < numberofastronauts; i++) {
+        astronauts.push({
+            name: `EV-${i+1}`,
+            oxygen: Math.floor(Math.random() * 100),
+            battery: Math.floor(Math.random() * 100),
+            co2: Math.floor(Math.random() * 1000),
+            heartRate: Math.floor(Math.random() * 100),
+            temperature: Math.floor(Math.random() * 100),
+            location: 'Mars',
+            fanSpeed: Math.floor(Math.random() * 100)
+        });
+    }
+    return astronauts
+}
+export const biometrics = readable(
+    getastros(),
     (set) => {
-        const interval = setInterval(() => {
-            set({
-                name: 'EV-1',
-                oxygen: Math.floor(Math.random() * 100),
-                battery: Math.floor(Math.random() * 100),
-                co2: Math.floor(Math.random() * 100),
-                heartRate: Math.floor(Math.random() * 100),
-                temperature: Math.floor(Math.random() * 100),
-                location: 'Mars',
-                fanSpeed: Math.floor(Math.random() * 100)
-            });
+        const interval = setInterval(() => {            
+            set(getastros());
         }, 1000);
 
         return () => clearInterval(interval);
