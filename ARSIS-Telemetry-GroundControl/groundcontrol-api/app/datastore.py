b7a8e70e68bd221a45fc88f4a73ed1af87dbb241
chuck <c.burnell@gmail.com>
2024-02-10 14:25:33 -0700

    queue len

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/datastore.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/datastore.py
@@ -1,5 +1,5 @@
 import asyncio
-import time
+from collections import deque
 
 from app.tss import get_from_tss, tss_keys
 from app.event import Event
@@ -19,7 +19,7 @@ class Datastore:
 
     async def append(self, key, value):
         if key not in self.cache:
-            self.cache[key] = []
+            self.cache[key] = deque(maxlen=100)
         self.cache[key].append(value)
 
     async def start_polling_key(self, key):
