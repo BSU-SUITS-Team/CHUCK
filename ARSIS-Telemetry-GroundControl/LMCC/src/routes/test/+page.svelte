72a4a11096946bb2e93c62d1bca0a33c0713410c
Aetheris743 <your_email@example.com>
2024-02-16 18:59:31 -0700

    rough draft

---
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/test/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { createWebSocketStore } from './teststore';
+	let remoteData = createWebSocketStore('ws://localhost:8181/ws/events');
+</script>
+
+{JSON.stringify($remoteData)}
+
+{#if $remoteData}
+	<div>
+		Time = {$remoteData.time}
+	</div>
+{/if}
