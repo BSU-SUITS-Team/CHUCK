7f82fb15b4a2ec159f10ce90a2df4dc622b4c393
Aetheris743 <your_email@example.com>
2024-02-17 15:05:03 -0700

    update formatting and add caching

--- ARSIS-Telemetry-GroundControl/LMCC/src/routes/test/+page.svelte
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/test/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import { createWebSocketStore } from './teststore';
+	import { createWebSocketStore } from '$lib/datastore';
 	let remoteData = createWebSocketStore('ws://localhost:8181/ws/events');
+
+	remoteData.subscribe(console.log);
 </script>
 
 {JSON.stringify($remoteData)}
