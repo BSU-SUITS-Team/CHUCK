60abda7c7b33c09f4d4552deee6497bc7277f9ab
Aetheris743 <your_email@example.com>
2024-03-30 12:03:34 -0600

    Removed Old Map

--- ARSIS-Telemetry-GroundControl/LMCC/src/routes/map/+page.svelte
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/map/+page.svelte
@@ -2,13 +2,3 @@
 	import Panview from './Panview.svelte';
 	import img from '$lib/images/test.svg';
 </script>
-
-<Panview
-	image={img}
-	initalSize={3}
-	minScale={2}
-	maxScale={20}
-	xRange={[-1000, 0]}
-	yRange={[-1000, 0]}
-	initalPosition={[-1300, -900]}
-/>
