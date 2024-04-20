7c7b2a00fa9921932dac9a0b86d1732e64ba92e1
Aetheris743 <your_email@example.com>
2024-04-20 15:04:24 -0600

    Fmt

--- ARSIS-Telemetry-GroundControl/LMCC/src/routes/hololenstest/+page.svelte
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/hololenstest/+page.svelte
@@ -1,9 +1,12 @@
 <script>
-	import { Input, Video } from "flowbite-svelte";
+	import { Input, Video } from 'flowbite-svelte';
 
-	let ipaddr = "192.168.125.50";
-
+	let ipaddr = '192.168.125.50';
 </script>
 
 <Input bind:value={ipaddr} />
-<Video src="http://{ipaddr}/api/holographic/stream/live_high.mp4" controls trackSrc="live_high.mp4" />
+<Video
+	src="http://{ipaddr}/api/holographic/stream/live_high.mp4"
+	controls
+	trackSrc="live_high.mp4"
+/>
