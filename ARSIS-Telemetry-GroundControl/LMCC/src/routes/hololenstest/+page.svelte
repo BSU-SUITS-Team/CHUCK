43e1615388afa637c939c9f9c3e7b79ed681efaa
Aetheris743 <your_email@example.com>
2024-04-20 15:00:36 -0600

    Added Video feed from Hololens

---
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/hololenstest/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Input, Video } from "flowbite-svelte";
+
+	let ipaddr = "192.168.125.50";
+
+</script>
+
+<Input bind:value={ipaddr} />
+<Video src="http://{ipaddr}/api/holographic/stream/live_high.mp4" controls trackSrc="live_high.mp4" />
