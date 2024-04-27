0d178fa35933ab85b76a0d5ae6bf2d83146063f1
Aetheris743 <your_email@example.com>
2024-04-27 12:55:28 -0600

    Small cleanup

--- ARSIS-Telemetry-GroundControl/LMCC/src/routes/Sidebar.svelte
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/Sidebar.svelte
@@ -4,33 +4,22 @@
 		Sidebar,
 		SidebarGroup,
 		SidebarItem,
-		SidebarBrand,
 		SidebarWrapper,
 		SidebarDropdownItem,
 		SidebarDropdownWrapper
 	} from 'flowbite-svelte';
 	import {
-		ChartPieSolid,
 		MapPinAltSolid,
 		UserGroupSolid,
-		ExclamationCircleSolid,
 		AdjustmentsHorizontalSolid,
 		BookSolid
 	} from 'flowbite-svelte-icons';
-	let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 	$: activeUrl = $page.url.pathname;
 </script>
 
 <Sidebar {activeUrl}>
 	<SidebarWrapper>
 		<SidebarGroup>
-			<SidebarItem label="Dashboard" href="/">
-				<svelte:fragment slot="icon">
-					<ChartPieSolid
-						class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
 			<SidebarItem label="Biometrics" href="/biometrics">
 				<svelte:fragment slot="icon">
 					<AdjustmentsHorizontalSolid
@@ -38,7 +27,7 @@
 					/>
 				</svelte:fragment>
 			</SidebarItem>
-			<SidebarDropdownWrapper label="Entities" isOpen={true}>
+			<SidebarDropdownWrapper label="Camera Feeds" isOpen={true}>
 				<svelte:fragment slot="icon">
 					<UserGroupSolid
 						class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -63,20 +52,6 @@
 					<BookSolid
 						class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
 					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Events" {spanClass}>
-				<svelte:fragment slot="icon">
-					<ExclamationCircleSolid
-						class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-				<svelte:fragment slot="subtext">
-					<span
-						class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"
-					>
-						3
-					</span>
 				</svelte:fragment>
 			</SidebarItem>
 		</SidebarGroup>
