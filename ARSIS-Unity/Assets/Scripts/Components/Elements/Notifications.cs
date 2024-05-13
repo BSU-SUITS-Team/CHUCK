6fe84d9832c03ddfa72e8bb88b2957ace4d8cd9d
Digno JR Teogalbo <dignojrteogalbo@gmail.com>
2024-05-12 22:28:06 -0600

    timer

--- ARSIS-Unity/Assets/Scripts/Components/Elements/Notifications.cs
+++ ARSIS-Unity/Assets/Scripts/Components/Elements/Notifications.cs
@@ -3,11 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.InputSystem;
 
-public class Notifications : MonoBehaviour
+public class Notifications : MonoBehaviour, IRenderable
 {
     [SerializeField] TextMeshProUGUI connectionText;
+    [SerializeField] TextMeshProUGUI missionTimer;
     [SerializeField] TextMeshProUGUI evaText;
+    private static string key = "eva";
+    private EVA timer = null;
+    private bool changed = true;
+
+    void IRenderable.Render(List<BaseArsisEvent> data)
+    {
+        if (data.Last() is EVA recent)
+            timer = recent;
+        changed = true;
+    }
 
     void Update()
     {
@@ -16,5 +29,26 @@ public class Notifications : MonoBehaviour
         connectionText.text = status;
         int eva = eventManager.Eva;
         evaText.text = "EVA" + eva;
+        if (!changed || timer == null) return;
+        string timerText = "In Progress";
+        if (timer.data.paused) timerText = "Paused";
+        if (timer.data.completed) timerText = "Completed";
+        float h = Mathf.Floor(timer.data.total_time / 3600);
+        float m = Mathf.Floor(timer.data.total_time % 3600 / 60);
+        float s = Mathf.Floor(timer.data.total_time % 3600 % 60);
+        missionTimer.text = timerText + $"\n{h}:{m}:{s}";
+        changed = false;
+    }
+
+    void Start()
+    {
+        EventDatastore eventDatastore = EventDatastore.Instance;
+        eventDatastore.AddHandler(key, this);
+    }
+
+    void OnDestroy()
+    {
+        EventDatastore eventDatastore = EventDatastore.Instance;
+        eventDatastore.RemoveHandler(key, this);
     }
 }
