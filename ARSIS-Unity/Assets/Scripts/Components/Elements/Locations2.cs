74fbd902c88a2a7c5883177a61b7957081ce2962
ainsley37 <ainsleyiwersen@u.boisestate.edu>
2024-05-14 10:36:21 -0500

    Created handler for pins

--- ARSIS-Unity/Assets/Scripts/Components/Elements/Locations2.cs
+++ ARSIS-Unity/Assets/Scripts/Components/Elements/Locations2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using ARSIS.EventManager;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Linq;
 
 public class Locations2 : MonoBehaviour, IRenderable
 {
@@ -10,13 +11,31 @@ public class Locations2 : MonoBehaviour, IRenderable
     [SerializeField] GameObject locationDisplay;
     private const string key = "imu";
     private const string key1 = "rover";
+    private const string key2 = "pins";
     private List<BaseArsisEvent> locations = new List<BaseArsisEvent>();
+    private List<BaseArsisEvent> roverData = new List<BaseArsisEvent>();
+    private List<BaseArsisEvent> pinsData = new List<BaseArsisEvent>();
     private bool changed = true;
 
     public void Render(List<BaseArsisEvent> data)
     {
-        locations = data; //storing list of locations from event system
+        //locations = data; //storing list of locations from event system
         changed = true;
+        if (data.FirstOrDefault() is IMU)
+        {
+            locations = data; 
+            return; 
+        }
+        if (data.FirstOrDefault() is Rover)
+        {
+            roverData = data; 
+            return; 
+        }
+        if (data.FirstOrDefault() is Pins)
+        {
+            pinsData = data; 
+            return; 
+        }
     }
 
     void Start()
@@ -24,13 +43,16 @@ public class Locations2 : MonoBehaviour, IRenderable
         EventDatastore eventDatastore = EventDatastore.Instance;
         eventDatastore.AddHandler(key, this); //imu 
         eventDatastore.AddHandler(key1, this); //rover
+        eventDatastore.AddHandler(key2, this); //pins
     }
 
     void OnDestroy()
     {
         EventDatastore eventDatastore = EventDatastore.Instance;
-        eventDatastore.RemoveHandler(key, this);//imu
-        eventDatastore.RemoveHandler(key1, this);//rover
+        eventDatastore.RemoveHandler(key, this); //imu
+        eventDatastore.RemoveHandler(key1, this); //rover
+        eventDatastore.RemoveHandler(key2, this); //pins
+
     }
 
     void Update()
@@ -51,6 +73,8 @@ public class Locations2 : MonoBehaviour, IRenderable
         // scrollArea.SetEntries(entries);
         changed = false;
         Debug.Log(((IMU)locations[locations.Count -1]));
+        Debug.Log(((Rover)roverData[roverData.Count -1]));
+        Debug.Log(((Pins)pinsData[pinsData.Count -1]));
         //Debug.Log(((IMU)locations[locations.Count -1]).data.imu);
         
     }
--- ARSIS-Unity/Assets/Scripts/EventManager/EventTypes/BaseArsisEvent.cs
+++ ARSIS-Unity/Assets/Scripts/EventManager/EventTypes/BaseArsisEvent.cs
@@ -21,6 +21,7 @@ namespace ARSIS.EventManager
             { "uia", typeof(UIA ) },
             { "rover", typeof(Rover) },
             { "procedure", typeof(Procedure) },
+            { "pins", typeof(Pins) },
         };
 
         public string type;
