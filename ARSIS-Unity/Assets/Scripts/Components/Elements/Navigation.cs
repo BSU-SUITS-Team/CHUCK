175b2f3ff377ad124d305564a629fdf4d9ec14ff
Digno JR Teogalbo <dignojrteogalbo@gmail.com>
2024-05-15 01:16:03 -0600

    fix bugs

--- ARSIS-Unity/Assets/Scripts/Components/Elements/Navigation.cs
+++ ARSIS-Unity/Assets/Scripts/Components/Elements/Navigation.cs
@@ -9,14 +9,12 @@ using MixedReality.Toolkit.UX.Experimental;
 public class Navigation : MonoBehaviour
 {
     [SerializeField] RectTransform image;
-    [SerializeField] GameObject map;
-    [SerializeField] GameObject imageObj;
-    [SerializeField] RectTransform mapTrans;
+    [SerializeField] RectTransform map;
     [SerializeField] GameObject pinPrefab;
     private float maxScale = 2f;
     private float minScale = 0.1f;
     private float pinSize = 5f;
-    private float pinScale = 0.2f;
+    private float pinScale = 1.33f;
 
     public void adjustScale(float adjust)
     {
@@ -29,19 +27,19 @@ public class Navigation : MonoBehaviour
     private void CreatePin(Vector2 anchored)
     {
         GameObject pin = Instantiate(pinPrefab);
-        pin.transform.SetParent(map.transform, false);
+        pin.transform.SetParent(map, false);
         RectTransform pinTrans = pin.GetComponent<RectTransform>();
-        pinTrans.localScale = Vector3.one * pinScale;
         pinTrans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, pinSize);
         pinTrans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, pinSize);
         pinTrans.anchoredPosition = anchored;
-        pin.transform.SetParent(image.transform, true);
+        pin.transform.SetParent(image, true);
+        pinTrans.localScale = Vector3.one * pinScale;
     }
 
     private Vector2 CalculateAnchor(Vector3 hit)
     {
         Vector3[] corners = new Vector3[4];
-        image.GetWorldCorners(corners);
+        map.GetWorldCorners(corners);
         Vector3 bottomLeft = corners[0];
         Vector3 topLeft = corners[1];
         Vector3 topRight = corners[2];
@@ -50,7 +48,8 @@ public class Navigation : MonoBehaviour
         Vector3 height = bottomLeft - topLeft;
         float widthFactor = Vector3.Dot(direction, width) / width.magnitude / width.magnitude;
         float heightFactor = Vector3.Dot(direction, height) / height.magnitude / height.magnitude;
-        return new Vector2(widthFactor * mapTrans.sizeDelta.x, heightFactor * -mapTrans.sizeDelta.y);
+        Debug.Log($"x: {widthFactor * map.sizeDelta.x}, y: {heightFactor * -map.sizeDelta.y}");
+        return new Vector2(widthFactor * map.sizeDelta.x, heightFactor * -map.sizeDelta.y);
     }
 
     public void HandleSelect(SelectExitEventArgs e)
@@ -60,7 +59,8 @@ public class Navigation : MonoBehaviour
         Transform trans = interactor.GetAttachTransform(interactable).parent;
         RaycastHit hit;
         if (!Physics.Raycast(trans.position, trans.forward, out hit)) return;
-        if (hit.transform.gameObject != map.transform.gameObject) return;
+        if (hit.transform.gameObject != map.gameObject) return;
+        Debug.Log($"hit: {hit.point}");
         Vector2 anchored = CalculateAnchor(hit.point);
         CreatePin(anchored);
     }
