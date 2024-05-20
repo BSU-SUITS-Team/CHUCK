f71fdf3eeeb2041a3ede487e4c181dfe8cd4bbfd
Aetheris743 <your_email@example.com>
2024-05-19 23:19:56 -0600

    cleanup

---
+++ ARSIS-Telemetry-GroundControl/LMCC/src/routes/hololenstest/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Input, Video } from 'flowbite-svelte';
+
+	let ipaddr = '192.168.137.75';
+
+</script>
+
+<Input bind:value={ipaddr} />
+<Video
+	src="http://{ipaddr}/api/holographic/stream/live_high.mp4"
+	controls
+	trackSrc="live_high.mp4"
+/>
