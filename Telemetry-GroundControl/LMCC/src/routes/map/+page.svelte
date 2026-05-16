f85539b130fccb09578f9ce987650d5ed7d55585
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2026-05-16 13:02:23 -0600

    update map view

--- Telemetry-GroundControl/LMCC/src/routes/map/+page.svelte
+++ Telemetry-GroundControl/LMCC/src/routes/map/+page.svelte
@@ -3,12 +3,4 @@
 	import img from '$lib/images/rockyard_map_geo.jpg';
 </script>
 
-<Panview
-	image={img}
-	initalSize={3}
-	minScale={0.6}
-	maxScale={20}
-	xRange={[-1000, 0]}
-	yRange={[-1000, 0]}
-	initalPosition={[-1300, -900]}
-/>
+<Panview image={img} />
